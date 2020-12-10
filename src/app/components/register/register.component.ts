f36cf90cbe2cb8372ae17c2a3a4ab07e86d53773
Domiviv <vivien.nechelput@gmail.com>
2020-12-10 22:53:04 +0100

    issues resolved, progressing with the login and register component adding must-watch.validator.ts for password and password confirmation

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,15 +1,79 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {User} from '../../models/User.model';
+import {MustMatch} from '../../../_helpers/must-match.validator';
+import {Subscription} from 'rxjs';
+import {UserService} from '../../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.scss']
 })
-export class RegisterComponent implements OnInit {
+export class RegisterComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  registerForm: FormGroup;
+  newUser = new User();
+  submitted = false;
+  pwdMinLength = 4;
+  userSubscription: Subscription;
+
+  constructor(private formBuilder: FormBuilder,
+              private userService: UserService,
+              private router: Router) { }
 
   ngOnInit(): void {
+    this.initForm();
   }
 
+  initForm(): void {
+    this.registerForm = this.formBuilder.group({
+      lastname: ['', Validators.required],
+      firstname: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]],
+      address: ['', Validators.required],
+      number: ['', Validators.required],
+      postcode: ['', Validators.required],
+      city: ['', Validators.required],
+      newsletter: [''],
+      pwd: ['', [Validators.required, Validators.minLength(this.pwdMinLength)]],
+      pwdconf: ['', Validators.required]
+    }, {
+      validator: MustMatch('pwd', 'pwdconf')
+    });
+  }
+
+  // convenience getter for easy access to form fields
+  // tslint:disable-next-line:typedef
+  get f() { return this.registerForm.controls; }
+
+  onSubmit(): void {
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.registerForm.invalid) {
+      return;
+    }
+
+    this.newUser.lastName = this.registerForm.value.lastname;
+    this.newUser.firstName = this.registerForm.value.firstname;
+    this.newUser.email = this.registerForm.value.email;
+    this.newUser.address = this.registerForm.value.address + ', ' + this.registerForm.value.number;
+    this.newUser.address2 = this.registerForm.value.postcode + ', ' + this.registerForm.value.city;
+    this.newUser.pwd = this.registerForm.value.pwd;
+    this.newUser.newsletter = this.registerForm.value.newsletter;
+
+    this.userSubscription = this.userService.newCustomer(this.newUser).subscribe(
+      () => {
+        alert('Félicitation, votre compte a été créé.\n\nVous serez redirigé vers la page de connexion après avoir cliqué sur "Ok"');
+        this.router.navigate(['login']);
+      }
+    );
+
+  }
+
+  ngOnDestroy(): void {
+    if (this.userSubscription) { this.userSubscription.unsubscribe(); }
+  }
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import {Observable} from 'rxjs';
+import {API_URL, HTTP_OPTIONS} from '../app.constants';
+import {HttpClient} from '@angular/common/http';
+import {User} from '../models/User.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) { }
+
+  newCustomer(newUser: User): Observable<User> {
+    return this.httpClient.post<User>(API_URL + 'user/customer/add', newUser, HTTP_OPTIONS);
+  }
 }
